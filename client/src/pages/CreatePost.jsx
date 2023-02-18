8c71a410438b166e8002f577f8955fdf1450775c
Jaco-Miras <58285015+Jaco-Miras@users.noreply.github.com>
2023-02-18 17:10:21 +0800

    final commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -24,16 +24,13 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
 
-        const response = await fetch(
-          "https://image-studio2.onrender.com/api/v1/studio",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ prompt: form.prompt }),
-          }
-        );
+        const response = await fetch("http://localhost:8080/api/v1/studio", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
 
         const data = await response.json();
 
@@ -55,16 +52,13 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch(
-          "https://image-studio2.onrender.com/api/v1/post",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(form),
-          }
-        );
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        });
 
         await response.json();
         navigate("/");
